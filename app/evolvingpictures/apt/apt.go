cacdc3cb785d0ed13b90a8c88202a273a4243372
Keith Lashley <lashleykeith@gmail.com>
2019-03-25 14:33:59 +0900

    Add files via upload

--- app/evolvingpictures/apt/apt.go
+++ app/evolvingpictures/apt/apt.go
@@ -1,7 +1,9 @@
 package apt
 
 import (
+	"github.com/jackmott/noise"
 	"math"
+	"strconv"
 	//. "github.com/jackmott/gameswithgo-public/evolvingpictures/apt"
 )
 
@@ -38,6 +40,42 @@ func (op *OpSin) String() string {
 	return "( Sin " + op.Child.String() + ")"
 }
 
+type OpCos struct {
+	SingleNode
+}
+
+func (op *OpCos) Eval(x, y float32) float32 {
+	return float32(math.Cos(float64(op.Child.Eval(x, y))))
+}
+
+func (op *OpCos) String() string {
+	return "( Cos " + op.Child.String() + ")"
+}
+
+type OpAtan struct {
+	SingleNode
+}
+
+func (op *OpAtan) Eval(x, y float32) float32 {
+	return float32(math.Atan(float64(op.Child.Eval(x, y))))
+}
+
+func (op *OpAtan) String() string {
+	return "( Atan " + op.Child.String() + ")"
+}
+
+type OpNoise struct {
+	DoubleNode
+}
+
+func (op *OpNoise) Eval(x,y float32) float32{
+	return 80*noise.Snoise2(op.LeftChild.Eval(x,y), op.RightChild.Eval(x,y)) - 2.0
+}
+
+func (op *OpNoise) String() string{
+	return "( SimplexNoise " + op.LeftChild.String() +" " + op.RightChild.String() + " ) "
+}
+
 type OpPlus struct {
 	DoubleNode
 }
@@ -47,8 +85,58 @@ func (op *OpPlus) Eval(x, y float32) float32 {
 }
 
 func (op *OpPlus) String() string {
-	return "( + " + op.LeftChild.String() + " " + op.RightChild.String() + ")"
+	return "( + " + op.LeftChild.String() + " " + op.RightChild.String() + " )"
 }
+
+type OpMinus struct {
+	DoubleNode
+}
+
+func (op *OpMinus) Eval(x, y float32) float32 {
+	return op.LeftChild.Eval(x, y) - op.RightChild.Eval(x, y)
+}
+
+func (op *OpMinus) String() string {
+	return "( - " + op.LeftChild.String() + " " + op.RightChild.String() + " )"
+}
+
+type OpMult struct {
+	DoubleNode
+}
+
+func (op *OpMult) Eval(x, y float32) float32 {
+	return op.LeftChild.Eval(x, y) * op.RightChild.Eval(x, y)
+}
+
+func (op *OpMult) String() string {
+	return "( * " + op.LeftChild.String() + " " + op.RightChild.String() + " )"
+}
+
+type OpDiv struct {
+	DoubleNode
+}
+
+func (op *OpDiv) Eval(x, y float32) float32 {
+	return op.LeftChild.Eval(x, y) / op.RightChild.Eval(x, y)
+}
+
+func (op *OpDiv) String() string {
+	return "( / " + op.LeftChild.String() + " " + op.RightChild.String() + " )"
+}
+
+type OpAtan2 struct {
+	DoubleNode
+}
+
+func (op *OpAtan2) Eval(x, y float32) float32 {
+	return float32(math.Atan2(float64(y), float64(x)))
+}
+
+func (op *OpAtan2) String() string {
+	return "( Atan2 " + op.LeftChild.String() + " " + op.RightChild.String() + " )"
+}
+
+
 
 type OpX LeafNode
 
@@ -70,5 +158,18 @@ func (op *OpY) String() string {
 	return "Y"
 }
 
-// EP 43:39
+type OpConstant struct {
+	LeafNode
+	value float32
+}
+
+func (op *OpConstant) Eval(x, y float32) float32 {
+	return op.value
+}
+
+func (op *OpConstant) String() string {
+	return strconv.FormatFloat(float64(op.value), 'f', 9, 32)
+}
+
+// ep16 17:40
 // https://github.com/veandco/go-sdl2/
--- app/evolvingpictures/evolvingpictures.go
+++ app/evolvingpictures/evolvingpictures.go
@@ -1,27 +1,204 @@
+// http://gameswithgo.org/balloons/balloons.zip
+
+// ep15 103:00
 package main
+
+// Experiment! draw some crazy stuff!
+// Gist it next week and I'll show it off on stream
 
 import (
 	"fmt"
-
+	"time"
 	. "github.com/lashleykeith/golanggames/app/evolvingpictures/apt"
-	//. "github.com/jackmott/gameswithgo-public/evolvingpictures/apt"
-)
+	//. "github.com/jackmott/evolvingpictures/apt"
+	"github.com/veandco/go-sdl2/sdl")
 
-func main() {
-	x := &OpX{}
-	y := &OpY{}
-	plus := &OpPlus{}
-	sine := &OpSin{}
-	sine.Child = x
-	plus.LeftChild = sine
-	plus.RightChild = y
+const winWidth, winHeight, winDepth int = 1280, 720, 100
 
-	fmt.Println(plus.Eval(5, 2))
-	fmt.Println(plus)
+type audioState struct {
+	explosionBytes []byte
+	deviceID       sdl.AudioDeviceID
+	audioSpec      *sdl.AudioSpec
+}
+
+type mouseState struct {
+	leftButton  bool
+	rightButton bool
+	x, y        int
+}
+
+func getMouseState() mouseState {
+	mouseX, mouseY, mouseButtonState := sdl.GetMouseState()
+	leftButton := mouseButtonState & sdl.ButtonLMask()
+	rightButton := mouseButtonState & sdl.ButtonRMask()
+	var result mouseState
+	result.x = int(mouseX)
+	result.y = int(mouseY)
+	result.leftButton = !(leftButton == 0)
+	result.rightButton = !(rightButton == 0)
+	return result
+}
+
+
+
+type rgba struct {
+	r, g, b byte
+}
+
+func clear(pixels []byte) {
+	for i := range pixels {
+		pixels[i] = 0
+	}
+}
+
+func setPixel(x, y int, c rgba, pixels []byte) {
+	index := (y*winWidth + x) * 4
+	if index < len(pixels)-4 && index >= 0 {
+		pixels[index] = c.r
+		pixels[index+1] = c.g
+		pixels[index+2] = c.b
+	}
 
 }
 
-//42:00
+func pixelsToTexture(renderer *sdl.Renderer, pixels []byte, w, h int) *sdl.Texture {
+	tex, err := renderer.CreateTexture(sdl.PIXELFORMAT_ABGR8888, sdl.TEXTUREACCESS_STREAMING, int32(w), int32(h))
+	if err != nil {
+		panic(err)
+	}
+	tex.Update(nil, pixels, w*4)
+	return tex
+}
+
+func aptToTexture(redNode, greenNode, blueNode Node, w, h int, renderer *sdl.Renderer) *sdl.Texture {
+	// -1.0 and 1.0
+	scale := float32(255 / 2)
+	offset := float32(-1.0 * scale)
+	pixels := make([]byte, w*h*4)
+	pixelIndex := 0
+	for yi := 0; yi < h; yi++ {
+		y := float32(yi)/float32(h)*2 - 1
+		for xi := 0; xi < w; xi++ {
+			x := float32(xi)/float32(w)*2 - 1
+
+			r := redNode.Eval(x, y)
+			g := greenNode.Eval(x, y)
+			b := blueNode.Eval(x, y)
+
+
+			pixels[pixelIndex] = byte(r*scale - offset)
+			pixelIndex++
+			pixels[pixelIndex] = byte(g*scale - offset)
+			pixelIndex++
+			pixels[pixelIndex] = byte(b*scale - offset)
+			pixelIndex++
+			pixelIndex++
+
+		}
+	}
+	return pixelsToTexture(renderer, pixels, w, h)
+}
+
+func main() {
+	sdl.LogSetAllPriority(sdl.LOG_PRIORITY_VERBOSE)
+	err := sdl.Init(sdl.INIT_EVERYTHING)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer sdl.Quit()
+
+	window, err := sdl.CreateWindow("Evolving Pictures", 200, 200,
+		int32(winWidth), int32(winHeight), sdl.WINDOW_SHOWN)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer window.Destroy()
+
+	renderer, err := sdl.CreateRenderer(window, -1, sdl.RENDERER_ACCELERATED)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer renderer.Destroy()
+
+	// Note, run "go get -u github.com/veandco/go-sdl2/sdl" to get the latest sdl2 bindings
+	// which fixes an unecessary parameter in the LoadWAV call
+	/*explosionBytes, audioSpec := sdl.LoadWAV("explode.wav")
+	audioID, err := sdl.OpenAudioDevice("", false, audioSpec, nil, 0)
+	if err != nil {
+		panic(err)
+	}
+	defer sdl.FreeWAV(explosionBytes)
+
+	audioState := audioState{explosionBytes, audioID, audioSpec}
+	*/
+	
+	sdl.SetHint(sdl.HINT_RENDER_SCALE_QUALITY, "1")
+
+	
+	var elapsedTime float32
+	currentMouseState := getMouseState()
+	// prevMouseState := currentMouseState
+
+	x := &OpX{}
+	y := &OpY{}
+	sine := &OpSin{}
+	noise := &OpNoise{}
+	atan2 := &OpAtan2{}
+	plus := &OpPlus{}
+	atan2.LeftChild = x
+	atan2.RightChild = noise
+
+	sine.Child = atan2
+	plus.LeftChild = x
+	plus.RightChild = sine
+
+	tex := aptToTexture(plus, plus, plus, 640, 480, renderer)
+
+
+	for {
+		frameStart := time.Now()
+
+		currentMouseState = getMouseState()
+
+		for event := sdl.PollEvent(); event != nil; event = sdl.PollEvent() {
+			switch e := event.(type) {
+			case *sdl.QuitEvent:
+				return
+			case *sdl.TouchFingerEvent:
+				if e.Type == sdl.FINGERDOWN {
+					touchX := int(e.X * float32(winWidth))
+					touchY := int(e.Y * float32(winHeight))
+					currentMouseState.x = touchX
+					currentMouseState.y = touchY
+					currentMouseState.leftButton = true
+				}
+			}
+		}
+
+		renderer.Copy(tex, nil, nil)
+
+		
+		renderer.Present()
+		elapsedTime = float32(time.Since(frameStart).Seconds() * 1000)
+		//	fmt.Println("ms per frame:", elapsedTime)
+		if elapsedTime < 5 {
+			sdl.Delay(5 - uint32(elapsedTime))
+			elapsedTime = float32(time.Since(frameStart).Seconds() * 1000)
+		}
+
+		// prevMouseState = currentMouseState
+	}
+
+}
+
+// ep16 17:40
+//https://wiki.libsdl.org/SDL_GetMouseState
+
+//https://wiki.libsdl.org/SDL_TouchFingerEvent
+
 
 /// 63
 /*
